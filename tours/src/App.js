ab705c746b285a2c4ce06cfab3d13b05c45bbc69
somidhroy <somidhroy@gmail.com>
2022-10-28 20:02:38 +0530

    files added

--- tours/src/App.js
+++ tours/src/App.js
@@ -3,8 +3,6 @@ import Card from './Card'
 import data from './data'
 
 function App() {
-
-
     const cards = data.map(item => {
       return (
         <Card 
@@ -16,6 +14,7 @@ function App() {
       />
       )
     })
+
 
 
   return (
--- tours/src/Card.jsx
+++ tours/src/Card.jsx
@@ -3,20 +3,50 @@ import { useState } from 'react'
 
 const Card = (props) => {
 
+    const [visible, setVisible] = useState(true)
+
+
+    function handleClick() {
+        setVisible(false)
+    }
+
+    const ReadMore = ({ children }) => {
+        const text = children;
+        const [isReadMore, setIsReadMore] = useState(true)
+        const toggleReadMore = () => {
+            setIsReadMore(!isReadMore)
+        }
+
+        return (
+            <p>
+                {isReadMore ? text.slice(0, 150) : text}
+                <span onClick={toggleReadMore} className="text-[#49a6e9] cursor-pointer">
+                    {isReadMore ? "...Read More" : " Show Less"}
+                </span>
+            </p>
+        )
+    }
+
     return (
+        <>
+        {
+            visible &&
         <div className='w-[40em] mb-20 shadow-2xl'>
-            <img src={props.img} alt="" />
-            <div className='p-10'>
-                <div className='flex item-center justify-between'>
-                    <h1>{props.about}</h1>
-                    <span className=''>{props.price}</span>
+            <img className='bg-center bg-cover w-full h-[20em]' src={props.img} alt="" />
+            <div className='p-10 flex flex-col item-center justify-center'>
+                <div className='flex item-center justify-between mb-10'>
+                    <h1 className='font-bold tracking-widest text-[#102A42]'>{props.about}</h1>
+                    <span className='text-[#49a6e9] font-bold bg-[#dbedfb] p-1 tracking-widest  '>{props.price}</span>
                 </div>
                 <div>
-                    <p>{props.text}</p>
+                    <ReadMore className='text-[#617d982] mb-10'>{props.text}</ReadMore>
                 </div>
-                <button>Not Interested</button>
+                <button onClick={handleClick} className='text-[#bb2525] border border-solid border-[#bb2525] w-48 h-8 m-auto cursor-pointer rounded-md  focus:outline-none'>Not Interested</button>
             </div>
         </div>
+        }
+            </>
+       
     )
 }
 
