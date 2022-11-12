028e6295837c4f29617890d57878e1f63047b044
somidhroy <somidhroy@gmail.com>
2022-11-12 21:22:47 +0530

    files added

--- Grocery-bud/src/App.jsx
+++ Grocery-bud/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './Card'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   const [count, setCount] = useState(0)
--- Grocery-bud/src/Card.jsx
+++ Grocery-bud/src/Card.jsx
@@ -1,48 +1,103 @@
-import React, { useState } from 'react'
+import { PersonalVideo } from '@mui/icons-material';
+import React, { useState, useEffect } from 'react'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Items from './items';
 
 function Card() {
 
-    const [item, setItem] = useState('')
-    const [addItem, setAddItem] = useState(false)
+    const [input, setInput] = useState('')
+    const [items, setItems] = useState([])
+    // const [message, setMessage] = useState('')
 
-    function handleClick(e) {
+    console.log(items)
+
+
+    function addItem(e) {
         e.preventDefault()
-        setAddItem(true)
+        input &&
+            setItems([...items, input])
+        setInput('')
     }
 
-
-    function ShowItem() {
-
-        return (
-            addItem &&
-            <div>
-                <p>{item}</p>
-                <div>
-                    <EditIcon />
-                    <DeleteIcon />
-                </div>
-            </div>
-
-        )
+    const deleteAll = () => {
+        setItems(prev => (
+            { ...prev },
+            prev.splice()
+        ))
     }
 
+    const deleteBtn = (idx) => {
+
+        setItems(prev => (
+            { ...prev },
+            prev.filter((items, id) => id !== idx)
+        ))
+
+        //     const updatedItems= items.filter((item, id) => {
+        //         return id !== idx
+        //     })
+        //    setItems(updatedItems)
+
+    }
+    const editBtn = (item, indx) => {
+        setInput(item)
+
+        setItems(prev => (
+            { ...prev },
+            prev.filter((items, id) => id !== indx)
+        ))
+        
+    }
+
+    //  let showMessage = useEffect(() => {
+    //         setMessage("items added")   
+    //         const timer = setTimeout(() => {
+    //            <p>{message}</p>
+    //           }, 1000);
+    //           return () => clearTimeout(timer);
+
+    //    },[addItem])
 
 
     return (
-        <div>
-            <h1>Grocery Bud</h1>
-            <form >
-                <input value={item} type="text" placeholder='e.g.eggs' onChange={(e) => setItem(e.target.value)} />
-                <button onClick={handleClick}>Submit</button>
+        <div className='bg-[#242424] p-10 w-[30em] rounded-lg'>
+            {/* <p>{showMessage}</p> */}
+            <h1 className='text-5xl font-bold mb-6'>Grocery Bud</h1>
+            <form  className='flex items-center justify-center'>
+                <input className='bg-[#ffffffde] h-8 px-2 focus:outline-none rounded-l-md text-black' value={input} type="text" placeholder='e.g.eggs' onChange={(e) => setInput(e.target.value)} />
+                <button onClick={addItem} className="bg-[#a5d5f8] py-[3px] px-3 text-[#222] rounded-r-md  text-center">Submit</button>
             </form>
 
-            <ShowItem />
+            <div>
+                {
+                    items.map((item, indx) => {
+                        return (
+                            <div className='flex items-center justify-between mt-5' key={indx}>
+                                <p className='tracking-widest'> {item}</p>
+                                <div className='flex items-center gap-1 '>
+                                    <button className='bg-transparent' onClick={() => editBtn(item, indx)}><EditIcon /></button>
+                                    <button className='bg-transparent' onClick={() => deleteBtn(indx)}><DeleteIcon /></button>
+                                </div>
+                            </div>
 
+                        )
+                    })
+                }
+                <button className='mt-5  bg-[#a5d5f8] text-[#222] py-1 px-2 text-center' onClick={deleteAll}>Delete All</button>
+
+            </div>
         </div>
     )
 }
 
 export default Card
+
+
+
+
+{/* <AddItems
+input={item}
+key={indx}
+deleteBtn={deleteBtn(indx)}
+editBtn={editBtn}
+/> */}
